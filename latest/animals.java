a520c82e2fdd0286bc788825a0a903109e0ab1dc
Amy <alc515@yahoo.com>
2016-11-29 09:20:24 -0500

    Update animals.java
    redefined croc logic

--- latest/animals.java
+++ latest/animals.java
@@ -14,10 +14,9 @@ public animals(){
 
 	public String place_croc(player me, Tile curTile, ArrayList<Tile> stack){
 		
-		boolean tiger_present = false;
-		boolean prey_present = false;
-		String place_croc= null;
 		for(int i=0; i< stack.size(); i++ ){
+			boolean tiger_present = false;
+			boolean prey_present = false;
 			Tile existingTile = stack.get(i);
 			char prey = existingTile.tileID[4];
 			if (  existingTile.meeple > 0 || existingTile.meeple < 10 ) {
@@ -32,13 +31,12 @@ public animals(){
 					prey_present = true;
 			}
 			if( tiger_present && prey_present && !existingTile.ownTile) {
-			    place_croc= " CROCODILE";
-                curTile.meeple = -1;
-                me.crocodiles--;
-                return place_croc;
-            }
+				curTile.meeple = -1;
+				me.crocodiles--;
+				return " CROCODILE";
+           		 }
 			
-		}
+		};
 		return null;
 	};
 	
@@ -66,11 +64,11 @@ public animals(){
 				String strID = String.valueOf(existingTile.tileID);
 				if ( !existingTile.ownTile && (existingTile.meeple > 9 || existingTile.meeple < 1) ) {
 					if (curTile.tileID[3] == 'L' ) {
-					    location= 5;
-                        curTile.meeple = location;
-                        me.tigers--;
-					    return place_tiger;
-                    }
+						location= 5;
+						curTile.meeple = location;
+						me.tigers--;
+						return place_tiger;
+					    }
 					else if ( strID == "TLJT-" ||
 						  strID == "TLJTP" ||
 						  strID == "JLTT-" ||
@@ -85,12 +83,11 @@ public animals(){
 							else if (curTile.rotation == 90 ) location = 2;
 							else if ( curTile.rotation == 180 ) location = 4;
 							else if ( curTile.rotation == 270 ) location = 8;
-							else
-                            {
-                                curTile.meeple = location;
+							else{
+								curTile.meeple = location;
 								me.tigers--;
-                                return place_tiger;
-                            }
+								return place_tiger;
+                            				}
 					}
 					else if ( strID == "TLLT-" || strID == "TLLTB"  ){
 						if(curTile.rotation == 0 ) location = 9; 
@@ -98,10 +95,10 @@ public animals(){
 						else if ( curTile.rotation == 180 ) location = 1;
 						else if ( curTile.rotation == 270 ) location = 7;
 						else {
-                            curTile.meeple = location;
+                            				curTile.meeple = location;
 							me.tigers--;
-						    return place_tiger;
-                        }
+						   	 return place_tiger;
+                       				 }
 						
 					}
 					else if ( strID == "LJTJ-" ||
@@ -114,17 +111,17 @@ public animals(){
 							else if ( curTile.rotation == 180 ) location = 8;
 							else if ( curTile.rotation == 270 ) location = 6;
 							else {
-                                curTile.meeple = location;
+                               					curTile.meeple = location;
 								me.tigers--;
-							    return place_tiger;
-                            }
+							    	return place_tiger;
+                           				 }
 					}
 					else {
-                        location = 5;
-                        curTile.meeple = location;
+						location = 5;
+						curTile.meeple = location;
 						me.tigers--;
-                        return place_tiger;
-                    }
+						return place_tiger;
+                   			 }
 				}
 			    }
 			}
